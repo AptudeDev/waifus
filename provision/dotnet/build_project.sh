f01f036e46217f121690330c33d041c265a6d4ee
Carlos Betancourt <cbetancourt06@hotmail.com>
2022-02-08 12:53:23 -0600

    Se corrige error en el archivo build_project

--- provision/dotnet/build_project.sh
+++ provision/dotnet/build_project.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -v
 cowsay "Build project $1"
-cd $1echo "cargando variables de $2"
+cd $1
+echo "cargando variables de $2"
 cat $2
 export $(grep -v '^#' $2 | xargs -d '\n')
 PATH="$PATH:/root/.dotnet/tools"
